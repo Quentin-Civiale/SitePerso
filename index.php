d7035360c7b2f33ba0a9e625877daf1e3e69617a
Quentin Civiale <quentin.civiale@gmail.com>
2018-01-22 19:52:08 +0100

    Fichiers principaux du site

---
+++ index.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css" />
+    <!-- Pour tout le monde -->
+    <link rel="icon" type="image/x-icon" href="Images/favicon.ico" />
+    <title>Quentin Civiale</title>
+
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="materialize-v0.98.2/materialize/css/materialize.min.css" media="screen,projection" />
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+
+    <header>
+        <div class="row">
+            <div class="col s12"></div>
+        </div>
+    </header>
+
+    <a id="haut"></a>
+
+    <section>
+        <div class="row">
+            <div class="card-panel hoverable"><img class="circle responsive-img" src="Images/Image%202.jpg">
+
+                <aside class="info">
+                    <!--<h2>(Site en construction)</h2>-->
+                    <a href="index.php">
+                        <h1>Quentin Civiale</h1>
+                    </a>
+                    <h3>26 ans</h3>
+                    <h5>Etudiant Chef de Projet Multimédia - Développement WEB<br/>à <a href="https://openclassrooms.com/paths/chef-de-projet-multimedia-developpement" target="_blank">OpenClassrooms</a></h5>
+                    <p>
+                        <a class="btn btn-floating amber lighten-1 pulse" href="https://www.google.fr/maps/place/Les+Arbres,+63230+Chapdes-Beaufort/@45.9028198,2.8969605,17.25z/data=!4m8!1m2!2m1!1sles+arbres!3m4!1s0x47f73fa721f3b505:0x31478d7480adcb41!8m2!3d45.903412!4d2.8976?hl=fr" onclick="window.open(this.href); return false;"> <i class="material-icons">room</i> </a> Lieu-dit "Les Arbres" - 63230 Chapdes-Beaufort</p>
+                    <p>
+                        <a class="btn btn-floating amber pulse" href="#"> <i class="material-icons">phone</i> </a> <strong> Tél :</strong> 06 77 14 32 77</p>
+                    <p>
+                        <a class="btn btn-floating amber darken-2 pulse" href="mailto:quentin.civiale@gmail.com"> <i class="material-icons">email</i> </a> <strong> Mail :</strong> quentin.civiale@gmail.com</p>
+                    <p>
+                        <a class="btn btn-floating amber darken-4 pulse" href="https://www.linkedin.com/in/quentin-civiale/" onclick="window.open(this.href); return false;"> <i class="material-icons">person_pin</i> </a> Profil LinkedIn</p>
+                </aside>
+
+                <aside class="moreinfo">
+                    <a class="waves-effect waves-light btn-large amber" href="CV" target="_blank"><i class="material-icons left">list</i>CV</a>
+                </aside>
+
+            </div>
+        </div>
+    </section>
+
+
+    <section>
+        <div class="card-panel hoverable">
+            <article>
+
+                <h5>Projets réalisés dans le cadre de la formation :</h5>
+                <h5><strong>Chef de Projet Multimédia - Développement Web</strong> chez OpenClassrooms</h5>
+                <h6>(Tous les sites créés durant la formation sont fictifs et n'ont aucun lien avec de possibles sites internets.)</h6>
+
+                <br/>
+
+                <div class="row">
+
+                    <div class="col s12 m6">
+                        <div class="card amber darken-1">
+                            <div class="card-content white-text">
+                                <span class="card-title"><em>Projet 1</em></span>
+                                <p><strong>Créer et déployer un site en ligne</strong><br/>Vous devez créer un site web pour promouvoir les activités culturelles de votre ville ou village, à la demande de la mairie. On vous demande de créer un site qui respecte visuellement l’identité de la ville avec un certain nombre de fonctionnalités. La ville souhaite être en mesure de mettre à jour le contenu de son site sans avoir à faire appel à un prestataire dans le futur.</p>
+                            </div>
+                            <div class="card-action white">
+                                <a href="P1" target="_blank">Lien vers Projet 1</a>
+                                <a class="bouton_etat">
+                                    <a class="btn btn-floating light-green darken-4" title="Projet Validé"> <i class="material-icons">done</i> </a>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div class="col s12 m6">
+                        <div class="card amber darken-1">
+                            <div class="card-content white-text">
+                                <span class="card-title"><em>Projet 2</em></span>
+                                <p><strong>Initier et lancer un projet multimédia</strong><br/>Vous êtes développeur junior dans une agence web depuis 2 ans. Vous souhaitez maintenant monter en compétence sur la gestion de projet. Suite à votre formation, le directeur commercial de votre agence vous a chargé de répondre à votre premier appel d’offres. L'organisateur du salon du chocolat, souhaite faire développer un site web pour communiquer sur son évènement.</p>
+                            </div>
+                            <div class="card-action white">
+                                <a href="P2" target="_blank">Lien vers Projet 2</a>
+                                <a class="bouton_etat">
+                                    <a class="btn btn-floating light-green darken-4" title="Projet validé"> <i class="material-icons">done</i> </a>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+                <div class="row">
+
+                    <div class="col s12 m6">
+                        <div class="card amber darken-1">
+                            <div class="card-content white-text">
+                                <span class="card-title"><em>Projet 3</em></span>
+                                <p><strong>Créer un blog pour un écrivain</strong><br/>Vous venez de décrocher un contrat avec Jean Forteroche, acteur et écrivain. Il travaille actuellement sur son prochain roman, "Billet simple pour l'Alaska". Il souhaite innover et le publier par épisode en ligne sur son propre site. Seul problème : Jean n'aime pas WordPress et souhaite avoir son propre outil de blog, offrant des fonctionnalités plus simples. Vous allez donc devoir développer un moteur de blog en PHP et MySQL. Le livre de Jean Forteroche reste à écrire... mais il sera publié en ligne !</p>
+                            </div>
+                            <div class="card-action white">
+                                <a href="P3.2.2" target="_blank">Lien vers Projet 3</a>
+                                <a class="bouton_etat">
+                                    <a class="btn btn-floating light-green darken-4" title="Projet Validé"> <i class="material-icons">done</i> </a>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div class="col s12 m6">
+                        <div class="card amber darken-1">
+                            <div class="card-content white-text">
+                                <span class="card-title"><em>Projet 4</em></span>
+                                <p><strong>Développer un back-end pour un client</strong><br/>Le musée du Louvre vous a missionné pour un projet ambitieux : créer un nouveau système de réservation et de gestion des tickets en ligne pour diminuer les longues files d’attente et tirer parti de l’usage croissant des smartphones. L’interface doit être accessible aussi bien sur ordinateur de bureau que tablettes et smartphones, et utiliser pour cela un design responsive. L’interface doit être fonctionnelle, claire et rapide avant tout. L’objectif est de permettre aux visiteurs d’acheter un billet rapidement.</p>
+                            </div>
+                            <div class="card-action white">
+                                <a href="#">Projet en cours</a>
+                                <a class="bouton_etat">
+                                    <a class="btn btn-floating amber darken-4" title="Projet en cours"> <i class="material-icons">edit</i> </a>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+                <div class="row">
+
+                    <div class="col s12 m6">
+                        <div class="card grey lighten-1">
+                            <div class="card-content white-text">
+                                <span class="card-title"><em>Projet 5</em></span>
+                                <p><strong>Gestion de projet en équipe</strong><br/>Michel Dujardin est le fondateur de l’association NAO (Nos Amis les Oiseaux), regroupant les passionnés d’ornithologie. Il souhaite créer une application participative dans laquelle les particuliers pourraient indiquer où ils ont observé des oiseaux au cours de leurs promenades. Les participants prendront part à un programme de recherche visant à étudier les effets du climat, de l’urbanisation et de l’agriculture sur la biodiversité. Les utilisateurs principaux de l’application sont les particuliers et les naturalistes (professionnels) qui valident notamment les saisies sur l’application. Ces derniers ont exprimé leur intérêt pour pouvoir effectuer de la saisie sur le terrain et ouvrir les observations aux particuliers, grâce aux smartphones. Néanmoins, tout le monde n’étant pas équipé de smartphones, cela ne peut pas être l’unique méthode de saisie : une interface web sera aussi nécessaire.</p>
+                            </div>
+                            <div class="card-action white">
+                                <!--<a href="#">Lien vers Projet 5</a>-->
+                                <a class="bouton_etat">
+                                    <a class="btn btn-floating grey" title="Projet en attente"> <i class="material-icons">lock</i> </a>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+                    <div class="col s12 m6">
+                        <div class="card grey lighten-1">
+                            <div class="card-content white-text">
+                                <span class="card-title"><em>Projet 6</em></span>
+                                <p><strong>Contribuez à votre écosystème</strong><br/>Où que vous viviez, quelles que soient vos passions et vos connaissances, il y a forcément quelque chose que vous aimeriez améliorer autour de vous. Le but de ce projet, bien réel et concret, est de renvoyer l’ascenseur à la communauté autour de vous en réalisant un projet qui a un véritable impact social.<br/> Sujet 1 : Gérez un projet numérique Menez un projet numérique de la forme qui vous semblera la plus adaptée (réalisez un site wordpress ; codez un site web, ou application mobile…) pour répondre à un besoin autour de vous. Il peut s’agir d’un site web pour l’association de théâtre de votre ville, d’une application pour localiser et référencer les objets trouvés pendant un festival, etc. Prenez le temps de choisir un sujet qui vous touche et pour lequel vous ressentez un véritable besoin. Si vous n’avez pas d’idée, choisissez une association ou une organisation qui vous tient à coeur. Vous pouvez entre autres par exemple faire le site internet de l’association d’escalade de votre ville, ou celui d’un établissement que vous avez fréquenté, etc…<br/> Sujet 2 : Créer un cours en ligne (incubé sur le CourseLab d’OpenClassrooms) sur un outil ou une notion que vous affectionnez, qui vous paraîtrait utile pour d’autres élèves, qui pourrait servir de ressource pédagogique pour ceux qui apprennent le métier de Chef de Projet Multimédia.</p>
+                            </div>
+                            <div class="card-action white">
+                                <!--<a href="#">Lien vers Projet 6</a>-->
+                                <a class="bouton_etat">
+                                    <a class="btn btn-floating grey" title="Projet en attente"> <i class="material-icons">lock</i> </a>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </article>
+        </div>
+    </section>
+
+    <br/>
+
+    <section>
+        <div class="card-panel hoverable">
+            <h3>Compétences</h3><br/>
+
+
+            <div class="row">
+                <div class="col s12 m6">
+                    <h4>FrontEnd</h4><br/>
+
+                    <p>HTML</p>
+                    <p>80%</p>
+                    <div class="progress grey">
+                        <div class="determinate amber darken-1" style="width: 80% "></div>
+                    </div>
+
+                    <p>CSS</p>
+                    <p>70%</p>
+                    <div class="progress grey">
+                        <div class="determinate amber darken-1" style="width: 70% "></div>
+                    </div><br/>
+
+                    <p>Javascript</p>
+                    <p>en cours</p>
+                    <div class="progress grey">
+                        <div class="indeterminate amber darken-1"></div>
+                    </div><br/>
+                </div>
+
+                <div class="col s12 m6">
+                    <h4>BackEnd</h4><br/>
+
+                    <p>PHP</p>
+                    <p>70%</p>
+                    <div class="progress grey">
+                        <div class="determinate amber darken-1" style="width: 70% "></div>
+                    </div>
+
+                    <p>MySQL</p>
+                    <p>70%</p>
+                    <div class="progress grey">
+                        <div class="determinate amber darken-1" style="width: 70% "></div>
+                        <!--                        <div class="indeterminate amber darken-1"></div>-->
+                    </div><br/>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col s12 m6">
+                    <h4>Framework</h4><br/>
+
+
+                    <p>Bootstrap</p>
+                    <p>75%</p>
+                    <div class="progress grey">
+                        <div class="determinate amber darken-1" style="width: 75% "></div>
+                    </div><br/>
+
+                    <p>Materialize</p>
+                    <p>75%</p>
+                    <div class="progress grey">
+                        <div class="determinate amber darken-1" style="width: 75% "></div>
+                    </div><br/>
+
+                    <p>Symfony</p>
+                    <p>en cours</p>
+                    <div class="progress grey">
+                        <div class="indeterminate amber darken-1"></div>
+                    </div><br/>
+
+                    <p>jQuery</p>
+                    <p>en cours</p>
+                    <div class="progress grey">
+                        <div class="indeterminate amber darken-1"></div>
+                    </div><br/>
+                </div>
+
+                <div class="col s12 m6">
+                    <h4>CMS</h4><br/>
+
+
+                    <p>WordPress</p>
+                    <p>75%</p>
+                    <div class="progress grey">
+                        <div class="determinate amber darken-1" style="width: 75% "></div>
+                    </div><br/>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+    <div class="buttonUp">
+        <a class="waves-effect waves-light btn grey" href="#haut" title="Remonter vers le haut"><i class="material-icons">present_to_all</i></a>
+    </div>
+
+
+
+    <footer class="page-footer amber darken-1">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">Quentin Civiale</h5>
+                    <p class="grey-text text-lighten-4"></p>
+                </div>
+                <!--<div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Mes Liens</h5>
+                    <ul>
+                        <div class="row">
+                            <div class="col s6">
+                                <li><a class="grey-text text-lighten-3" href="#!">CV</a></li>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col s6">
+                                <li><a class="grey-text text-lighten-3" href="#!">Projet 1</a></li>
+                                <li><a class="grey-text text-lighten-3" href="#!">Projet 2</a></li>
+                                <li><a class="grey-text text-lighten-3" href="#!">Projet 3</a></li>
+                            </div>
+                            <div class="col s6">
+                                <li><a class="grey-text text-lighten-3" href="#!">Projet 4</a></li>
+                                <li><a class="grey-text text-lighten-3" href="#!">Projet 5</a></li>
+                                <li><a class="grey-text text-lighten-3" href="#!">Projet 6</a></li>
+                            </div>
+                        </div>
+                    </ul>
+                </div>-->
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                © 2018 quentin-civiale.fr - Quentin Civiale - Tous droits réservés
+                <a class="grey-text text-lighten-4 right" href="mailto:quentin.civiale@gmail.com">Contactez-moi !</a>
+            </div>
+        </div>
+    </footer>
+
+
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+
+</html>
