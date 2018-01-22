77aeba889b292ff1fd6528be1e8abac8d0407eba
Quentin Civiale <quentin.civiale@gmail.com>
2018-01-22 19:45:59 +0100

    Fichiers principaux dossier P2

---
+++ P2/index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css" />
+    <!-- Pour tout le monde -->
+    <link rel="icon" type="image/x-icon" href="Images/favicon.ico" />
+    <title>Projet 2</title>
+
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="../materialize-v0.98.2/materialize/css/materialize.min.css" media="screen,projection" />
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+
+    <h1>Initier et lancer un projet Multimédia</h1>
+    <br/>
+
+    <section>
+        <article>
+            <h3>Scénario</h3>
+            <hr>
+            <p>Vous venez d'être embauché comme chef de projet dans l'agence web "Digitalizer".<br/> Pour votre première mission, votre directeur vous a chargé de reprendre le dossier d'un de vos collègues. Pour la passation de dossier, ce collègue vous a redigé "un brief".</p>
+            <br/>
+        </article>
+
+        <article>
+            <h3>Brief client</h3>
+            <hr>
+            <p>Frédéric Charain, organisateur du salon du chocolat de Paris, souhaite faire développer un site web pour communiquer sur son évènement ; celui-ci aura lieu dans 6 mois (à compter de la date de début du projet).<br/> Il souhaite pouvoir y présenter :</p>
+            <ul>
+                <li>- la liste des exposants;</li>
+                <li>- la carte des exposants;</li>
+                <li>- les intervenants prévus lors des tables rondes;</li>
+                <li>- la liste des actualités et des retombées presse.</li>
+            </ul>
+            <p>Il souhaite aussi que les billets d’entrée puissent être commandés directement en ligne sur le site.</p>
+            <br/>
+        </article>
+
+        <article>
+            <h3>Instructions</h3>
+            <hr>
+            <p>Dans un premier temps, vous initierez le projet en adressant au client :</p>
+            <ul>
+                <li>- Une proposition commerciale comprenant une note de cadrage ainsi qu'un devis.</li>
+            </ul>
+            <p>Une fois l’appel d’offre remporté, vous lancerez le projet en livrant à votre directeur :</p>
+            <ul>
+                <li>- un cahier des charges;</li>
+                <li>- un budget prévisionnel;</li>
+                <li>- un planning de réalisation sous forme de diagramme de Gantt</li>
+            </ul>
+            <p>Conseil : Veillez à formuler une solution qui réponde aux besoins du client et respecte son budget ; sans pour autant compromettre l’intérêt financier de votre agence. Autrement dit, ne vendez le projet ni trop cher, ni à perte.</p>
+            <br/>
+        </article>
+
+        <article>
+            <h3>Compétences à valider</h3>
+            <hr>
+            <ul>
+                <li>- Présenter une réponse à l'appel d'offres et rédiger un devis et le cahier des charges qui y est lié</li>
+                <li>- Elaborer les différents éléments d’un projet</li>
+                <li>- Rédiger un cahier des charges fonctionnel</li>
+                <li>- Rédiger le cahier des charges technique</li>
+                <li>- Construire un planning de réalisation</li>
+                <li>- Formaliser un document contenant le dossier de création, le cahier des charges et tous les documents utiles aux mises à jour et aux évolutions d’un projet</li>
+                <li>- Soutenir et argumenter ses propositions</li>
+                <li>- Rédiger un organigramme fonctionnel pour la réalisation d’un projet</li>
+                <li>- Mettre en place un diagramme organisationnel (Gantt, PERT, etc.) à l’aide de logiciels appropriés</li>
+            </ul>
+            <br/>
+        </article>
+    </section>
+
+    <h2>Liste des livrables créés lors de ce projet :</h2>
+
+    <section>
+        <article>
+            <h3>Documents principaux :</h3>
+            <ul>
+                <li><a href="PDF/Proposition%20Commerciale.pdf" target="_blank">Proposition commerciale</a></li>
+                <li><a href="PDF/Note%20de%20Cadrage.pdf" target="_blank">Note de cadrage</a></li>
+                <li><a href="PDF/Devis.pdf" target="_blank">Devis</a></li>
+                <li><a href="PDF/Cahier%20des%20Charges.pdf" target="_blank">Cahier des charges</a></li>
+                <li><a href="PDF/Budget%20pr%C3%A9visionnel.pdf" target="_blank">Budget prévisionnel</a></li>
+            </ul>
+        </article>
+        <br/>
+
+        <article>
+            <h3>Documents complémentaires :</h3>
+            <ul>
+                <li><a href="PDF/Organigramme%20fonctionnel.pdf" target="_blank">Organigramme fonctionnel</a></li>
+                <li><a href="PDF/Impact%20Mapping%20-%20%20Salon%20du%20Chocolat.pdf" target="_blank">Impact Mapping</a></li>
+                <li><a href="PDF/Maquette_Desktop.pdf" target="_blank">Maquette Desktop</a></li>
+                <li><a href="PDF/Maquette_Smartphone.pdf" target="_blank">Maquette Smartphone</a></li>
+                <li><a href="PDF/Plan%20de%20charge.pdf" target="_blank">Plan de charge</a></li>
+                <li><a href="PDF/Planning%20Pr%C3%A9visionnel.pdf" target="_blank">Planning prévisionnel</a></li>
+            </ul>
+        </article>
+    </section>
+    <br/>
+
+
+
+    <footer>
+        <h4>Important !</h4>
+        <h5>Tous les renseignements présents dans les documents listés ci-dessus sont fictifs, les entreprises nommées, réelles ou fictives, ne sont en aucun cas concernées par ce projet dans la vie réelle.</h5>
+    </footer>
+    <br/>
+
+
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="../js/materialize.min.js"></script>
+</body>
+
+</html>
